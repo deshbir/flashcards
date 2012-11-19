4d8551b1bbeab1683de7f97d2446532a3d3da62f
Deshbir Dugal <deshbir.dugal@me.com>
2012-11-19 16:33:05 +0530

    Adding New Domains

---
+++ grails-app/domain/com/pearson/hsc/Product.groovy
@@ -0,0 +1,33 @@
+package com.pearson.hsc
+
+class Product {
+
+	static hasMany = [topics: Topic]
+	
+	static mapping = {
+		topics lazy: false
+	}
+	
+	String name
+	String description
+	String thumbnail
+	String image
+	
+	static constraints = {
+	}
+	
+	
+	/***********************************
+	 * START Offline configurations
+	 ***********************************/
+	
+	def static String[] offlineCachedUrls() {
+		return  ["/api/discipline/1", 
+				 "/api/discipline/2", 
+				 "/api/discipline/3"];
+	}
+		
+	/******************************
+	 * END Offline configurations
+	 *******************************/
+}
---
+++ grails-app/domain/com/pearson/hsc/Topic.groovy
@@ -0,0 +1,30 @@
+package com.pearson.hsc
+
+class Topic {
+
+	String name
+	String audioTrack
+	String audioSequence
+	
+	static constraints = {
+	}
+	
+	
+	/***********************************
+	 * START Offline configurations
+	 ***********************************/
+	
+	def static String[] offlineCachedUrls() {
+		return  ["/api/discipline/1/product/1",
+				 "/api/discipline/1/product/2",
+				 "/api/discipline/1/product/3",
+				 "/api/discipline/2/product/11",
+				 "/api/discipline/2/product/12",
+				 "/api/discipline/2/product/13"];
+	}
+		
+	/******************************
+	 * END Offline configurations
+	 *******************************/
+
+}
