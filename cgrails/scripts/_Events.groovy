75256982a10f6ea29473050c2e844d64faebcde0
Deshbir Dugal <deshbir.dugal@me.com>
2012-12-01 15:26:59 +0530

    Cloud Support

--- cgrails/scripts/_Events.groovy
+++ cgrails/scripts/_Events.groovy
@@ -6,8 +6,15 @@ eventPackagingEnd = {kind->
 	if (!(Metadata.getCurrent().getApplicationName().equals("cgrails"))) {
 		includeTargets << new File("${cgrailsPluginDir}/scripts/DeployCss.groovy")
 		includeTargets << new File("${cgrailsPluginDir}/scripts/ValidateApp.groovy")
+		
 		validateApp()
-		//deployCSS()
+		
+		def classLoader = Thread.currentThread().contextClassLoader
+		classLoader.addURL(new File(classesDirPath).toURI().toURL())
+		def config = new ConfigSlurper(GrailsUtil.environment).parse(classLoader.loadClass('CgrailsConfig'))
+		if(!config.cgrails.cloudMode) {
+			deployCSS()
+		}	
 	}
 }
 
