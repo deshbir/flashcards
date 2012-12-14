5547bb75956136a964eb715e3e584f08fa26545e
manavmanocha <manav.manocha@comprotechnologies.com>
2012-12-14 15:55:05 +0530

    #43 : Adding timestamp in get user API to prevent caching

--- web-app/js/bb/user/usermodel.js
+++ web-app/js/bb/user/usermodel.js
@@ -2,16 +2,25 @@ UserModel = new function() {
 
 	var modelObj = null;
 	
-	var Model = Backbone.Model.extend({
+	var Model = Backbone.Model.extend({					
 					urlRoot: com.compro.cgrails.REQUEST_CONTEXT + "/api/user/"
 				});
 	
-	this.get = function(){
+	this.get = function() {
 		if (modelObj == null) {
 			modelObj = new Model();
+		}		
+		
+		if (typeof modelObj.id === 'undefined' || modelObj.id == null || modelObj.id == "") {
+			modelObj.id = new Date().getTime();
 		}
+		
 		return modelObj;
-    };
+    },
+    
+    this.destroy = function(){
+    	modelObj.id = null;
+    }
 	
 	
 };
