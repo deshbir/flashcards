9d02499704b06a74169e64db959f41004ae77b7b
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2012-12-16 16:32:08 +0530

    Removing adhoc patterns

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -33,7 +33,7 @@ grails.mime.types = [
 //grails.urlmapping.cache.maxsize = 1000
 
 // What URL patterns should be processed by the resources plugin
-grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
+//grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
 grails.resources.adhoc.includes = []
 grails.resources.adhoc.excludes = ["*"]
 
