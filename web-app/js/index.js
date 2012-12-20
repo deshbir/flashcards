04dc084923dd2393e633ddd190338a30e9b64bd0
manavmanocha <manav.manocha@comprotechnologies.com>
2012-12-20 20:54:08 +0530

    Updating cache version

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 6;
+ 	var version = 7;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
