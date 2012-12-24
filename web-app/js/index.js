c608b57ed6644515cdaa7f393461926008d5e7d6
manavmanocha <manav.manocha@comprotechnologies.com>
2012-12-24 19:17:00 +0530

    Do not use HTML5 appCache if not supported

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -581,16 +581,18 @@
 	 //This function adds a listener for cache update(if any).
 	 //If a update is found, it swaps the new cache and reloads the window.
 	function add_cache_listener() {
-		 window.applicationCache.addEventListener('updateready', function(e) {
-			if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-				// Browser downloaded a new app cache.
-				// Swap it in and reload the page to get the new hotness.
-				window.applicationCache.swapCache();
-				if (confirm('A new version/update of this App is available! Click "OK" to reload and update your application from the server.')) {
-					window.location.reload();
-				}
-			} 
-		});	
+		if (window.applicationCache) {
+			 window.applicationCache.addEventListener('updateready', function(e) {
+				if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+					// Browser downloaded a new app cache.
+					// Swap it in and reload the page to get the new hotness.
+					window.applicationCache.swapCache();
+					if (confirm('A new version/update of this App is available! Click "OK" to reload and update your application from the server.')) {
+						window.location.reload();
+					}
+				} 
+			});	
+		}	 
 	 }
  	
  	
