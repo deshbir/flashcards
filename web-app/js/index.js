793bdc437bdf6a8ddb20924461e0f8ac7013dbd9
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2012-12-26 14:22:33 +0530

    Version update.

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 11;
+ 	var version = 12;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
