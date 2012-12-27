5fc64bf440261e9eb652f6ab16fd1fabf72a3404
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2012-12-27 20:02:15 +0530

    Padding fix in admin and version updates.

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 16;
+ 	var version = 17;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
