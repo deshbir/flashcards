4be0c0ed316791390e21e13f316c3e6bf4dcf49d
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2012-12-31 19:05:49 +0530

    version update.

--- web-app/js/adminindex.js
+++ web-app/js/adminindex.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 29;
+ 	var version = 30;
  	
  	// Add separate Log Settings from Mascula for different logs.
  	var musculaLogSettingsConfig = {
--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 29;
+ 	var version = 30;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
