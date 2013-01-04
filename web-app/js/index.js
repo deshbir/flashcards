4ffcedb142168e41c07b20fc52f855e65b511605
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2013-01-04 15:30:09 +0530

    Fix for iphone layout - facebook button and footer

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 35;
+ 	var version = 36;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
