63dccdff40361d8c60bf88b796c5d358d4f64b36
manavmanocha <manav.manocha@comprotechnologies.com>
2013-01-04 12:05:19 +0530

    Changing version parameter to "v"

--- web-app/js/adminindex.js
+++ web-app/js/adminindex.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 33;
+ 	var version = 34;
  	
  	// Add separate Log Settings from Mascula for different logs.
  	var musculaLogSettingsConfig = {
