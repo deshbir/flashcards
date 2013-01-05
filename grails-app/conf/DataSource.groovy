9bab5dea9fe4eef834e4e0e26f424eb6907a80b5
Preeti Gupta <preeti.gupta@comprotechnologies.com>
2013-01-05 18:32:04 +0530

    Adding port in database url.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -68,7 +68,7 @@ environments {
 			
 				uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 		
-				url = "jdbc:postgresql://"+uri.host+uri.path
+				url = "jdbc:postgresql://"+uri.host + ":" + uri.port +uri.path
 				username = uri.userInfo.split(":")[0]
 				password = uri.userInfo.split(":")[1]
 				pooled = true
