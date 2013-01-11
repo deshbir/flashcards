df65a17117cb7ac0680ee7244284ae5df01171d6
harsh <harsh@Harsh.COMPRO>
2013-01-11 19:31:38 +0530

    #151 IE8 and other unsupported browsers - show warning message

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 40;
+ 	var version = 41;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
