bcb30775b1f25f754fb8b70c4cc9b6f46676b24a
charumaheswari <charu.maheswari@comprotechnologies.com>
2013-01-15 17:19:14 +0530

    Version Updates.

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -30,7 +30,7 @@
  
  com.compro.application.hsc = (function() {
  	
- 	var version = 46;
+ 	var version = 47;
  	var emailConfig = {
  			adminEmail : "deshbir.dugal@comprotechnologies.com", 
  			subject : "Pearson HSC Error Report",
